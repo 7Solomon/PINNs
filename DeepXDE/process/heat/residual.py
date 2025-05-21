4dffa06c685ba9b65dacd64c231a700292072f76
7Solomon <johannes-post@gmx.de>
2025-05-21 12:41:40 +0200

    added scaling to transient

--- DeepXDE/process/heat/residual.py
+++ DeepXDE/process/heat/residual.py
@@ -1,3 +1,4 @@
+from process.heat.scale import *
 import deepxde as dde
 from config import concreteData
 
@@ -5,7 +6,7 @@ def lp_residual(x,y):
     y_t=dde.grad.jacobian(y, x, i=0)[2]
     y_xx=dde.grad.hessian(y, x)
     y_yy=dde.grad.hessian(y, x, i=1)
-    return y_t - concreteData.alpha() * (y_xx + y_yy)
+    return y_t - (scale_alpha_x(concreteData.alpha())*y_xx + scale_alpha_y(concreteData.alpha())*y_yy)
 
 
 def steady_lp_residual(x, y):
